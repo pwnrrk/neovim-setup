1ec753310dcd8cb97464b69fd88223184d19573a
R.Phuwanat <r.phuwanat.dev@gmail.com>
2022-03-15 20:43:12 +0700

    update plugin & config

--- init.lua
+++ init.lua
@@ -5,7 +5,7 @@ cmd "source ~/.vim/autoload/plug.vim"
 cmd "source ~/.vim/vimrc/plugin.vim"
 cmd "source ~/.vim/vimrc/coc.vim"
 cmd "source ~/.vim/vimrc/mic.vim"
-require('ayu').colorscheme();
+require('ayu').colorscheme()
 cmd "hi Normal guibg=NONE ctermbg=NONE"
 cmd "hi clear LineNr"
 cmd "hi clear SignColumn"
@@ -14,82 +14,88 @@ require('bufferline').setup {
   options = {
     numbers = "ordinal",
     close_command = "bw",       -- can be a string | function, see "Mouse actions"
-      right_mouse_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
-        left_mouse_command = "buffer %d",    -- can be a string | function, see "Mouse actions"
-          middle_mouse_command = nil,          -- can be a string | function, see "Mouse actions"
-            -- NOTE: this plugin is designed with this icon in mind,
-            -- and so changing this is NOT recommended, this is intended
-            -- as an escape hatch for people who cannot bear it for whatever reason
-            indicator_icon = '▎',
-            buffer_close_icon = '',
-            modified_icon = '●',
-            close_icon = '',
-            left_trunc_marker = '',
-            right_trunc_marker = '',
-            --- name_formatter can be used to change the buffer's label in the bufferline.
-            --- Please note some names can/will break the
-            --- bufferline so use this at your discretion knowing that it has
-            --- some limitations that will *NOT* be fixed.
-            name_formatter = function(buf)  -- buf contains a "name", "path" and "bufnr"
-              -- remove extension from markdown files for example
-              if buf.name:match('%.md') then
-                return vim.fn.fnamemodify(buf.name, ':t:r')
-              end
-            end,
-            max_name_length = 18,
-            max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
-            tab_size = 18,
-            diagnostics = "coc",
-            diagnostics_update_in_insert = true,
-            diagnostics_indicator = function(count, level, diagnostics_dict, context)
-              local icon = level:match("error") and " " or " "
-              return " " .. icon .. count
-            end,
-            -- NOTE: this will be called a lot so don't do any heavy processing here
-              custom_filter = function(buf_number, buf_numbers)
-                -- filter out filetypes you don't want to see
-                if vim.bo[buf_number].filetype ~= "<i-dont-want-to-see-this>" then
-                  return true
-                end
-                -- filter out by buffer name
-                if vim.fn.bufname(buf_number) ~= "<buffer-name-I-dont-want>" then
-                  return true
-                end
-                -- filter out based on arbitrary rules
-                -- e.g. filter out vim wiki buffer from tabline in your work repo
-                if vim.fn.getcwd() == "<work-repo>" and vim.bo[buf_number].filetype ~= "wiki" then
-                  return true
-                end
-                -- filter out by it's index number in list (don't show first buffer)
-                if buf_numbers[1] ~= buf_number then
-                  return true
-                end
-              end,
-              offsets = {{filetype = "NvimTree", text = "File Explorer" , text_align = "left"}},
-              show_buffer_icons = true , -- disable filetype icons for buffers
-              show_buffer_close_icons = true,
-              show_close_icon = true,
-              show_tab_indicators = true,
-              persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
-              enforce_regular_tabs = true,
-              always_show_bufferline = true,
-            }
-          }
+    right_mouse_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
+    left_mouse_command = "buffer %d",    -- can be a string | function, see "Mouse actions"
+    middle_mouse_command = nil,          -- can be a string | function, see "Mouse actions"
+    -- NOTE: this plugin is designed with this icon in mind,
+    -- and so changing this is NOT recommended, this is intended
+    -- as an escape hatch for people who cannot bear it for whatever reason
+    indicator_icon = '▎',
+    buffer_close_icon = '',
+    modified_icon = '●',
+    close_icon = '',
+    left_trunc_marker = '',
+    right_trunc_marker = '',
+    --- name_formatter can be used to change the buffer's label in the bufferline.
+    --- Please note some names can/will break the
+    --- bufferline so use this at your discretion knowing that it has
+    --- some limitations that will *NOT* be fixed.
+    name_formatter = function(buf)  -- buf contains a "name", "path" and "bufnr"
+      -- remove extension from markdown files for example
+      if buf.name:match('%.md') then
+        return vim.fn.fnamemodify(buf.name, ':t:r')
+      end
+    end,
+    max_name_length = 18,
+    max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
+    tab_size = 18,
+    diagnostics = "coc",
+    diagnostics_update_in_insert = true,
+    diagnostics_indicator = function(count, level, diagnostics_dict, context)
+      local icon = level:match("error") and " " or " "
+      return " " .. icon .. count
+    end,
+    -- NOTE: this will be called a lot so don't do any heavy processing here
+    custom_filter = function(buf_number, buf_numbers)
+      -- filter out filetypes you don't want to see
+      if vim.bo[buf_number].filetype ~= "<i-dont-want-to-see-this>" then
+        return true
+      end
+      -- filter out by buffer name
+      if vim.fn.bufname(buf_number) ~= "<buffer-name-I-dont-want>" then
+        return true
+      end
+      -- filter out based on arbitrary rules
+      -- e.g. filter out vim wiki buffer from tabline in your work repo
+      if vim.fn.getcwd() == "<work-repo>" and vim.bo[buf_number].filetype ~= "wiki" then
+        return true
+      end
+      -- filter out by it's index number in list (don't show first buffer)
+      if buf_numbers[1] ~= buf_number then
+        return true
+      end
+    end,
+    offsets = {{filetype = "NvimTree", text = "File Explorer" , text_align = "left"}},
+    show_buffer_icons = true , -- disable filetype icons for buffers
+    show_buffer_close_icons = true,
+    show_close_icon = true,
+    show_tab_indicators = true,
+    persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
+    enforce_regular_tabs = true,
+    always_show_bufferline = true,
+  }
+}
+
+-- following options are the default
+-- each of these are documented in `:help nvim-tree.OPTION_NAME`
 require'nvim-tree'.setup {
-  disable_netrw       = true,
-  hijack_netrw        = true,
-  open_on_setup       = false,
-  ignore_ft_on_setup  = {},
-  auto_close          = false,
-  open_on_tab         = false,
-  hijack_cursor       = false,
-  update_cwd          = false,
-  update_to_buf_dir   = {
+  disable_netrw        = false,
+  hijack_netrw         = true,
+  open_on_setup        = false,
+  ignore_buffer_on_setup = false,
+  ignore_ft_on_setup   = {},
+  auto_close           = false,
+  auto_reload_on_write = true,
+  open_on_tab          = false,
+  hijack_cursor        = false,
+  update_cwd           = false,
+  hijack_unnamed_buffer_when_opening = false,
+  hijack_directories   = {
     enable = true,
     auto_open = true,
   },
   diagnostics = {
-    enable = false,
+    enable = true,
     icons = {
       hint = "",
       info = "",
@@ -120,7 +126,7 @@ require'nvim-tree'.setup {
     height = 30,
     hide_root_folder = false,
     side = 'left',
-    auto_resize = true,
+    preserve_window_proportions = false,
     mappings = {
       custom_only = false,
       list = {}
@@ -132,13 +138,40 @@ require'nvim-tree'.setup {
   trash = {
     cmd = "trash",
     require_confirm = true
-  }
+  },
+  actions = {
+    change_dir = {
+      enable = true,
+      global = false,
+    },
+    open_file = {
+      quit_on_open = true,
+      resize_window = false,
+      window_picker = {
+        enable = true,
+        chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+        exclude = {
+          filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame", },
+          buftype  = { "nofile", "terminal", "help", },
+        }
+      }
+    }
+  },
+  log = {
+    enable = false,
+    truncate = false,
+    types = {
+      all = false,
+      config = false,
+      git = false,
+    },
+  },
 }
 require'nvim-treesitter.configs'.setup {
   ensure_installed = "maintained",
   sync_install = false,
   highlight = {
     enable = true,
-      additional_vim_regex_highlighting = false,
-    },
-  }
+    additional_vim_regex_highlighting = false,
+  },
+}
