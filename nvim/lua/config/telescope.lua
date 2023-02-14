2ef6298255cc4d3871d7b41133d31d81d498f021
TTCL\phuwanat.r <phuwanat.r@ttcl.com>
2023-02-14 16:24:45 +0700

    change telescope theme

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -1,4 +1,16 @@
-require('telescope').setup()
+require('telescope').setup{
+	pickers = {
+    find_files = {
+      theme = "dropdown",
+    },
+		git_files = {
+      theme = "dropdown",
+		},
+		live_grep = {
+      theme = "dropdown",
+		}
+  },
+}
 
 vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<CR>')
 vim.keymap.set('n', '<leader>fg', '<cmd>Telescope git_files<CR>')
