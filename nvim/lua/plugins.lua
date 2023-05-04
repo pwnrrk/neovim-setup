5981120789acfcfa8776ee7e9f1a5b157174acdb
R.Phuwanat <phuwanat.r@ttcl.com>
2023-05-04 13:04:46 +0700

    update order

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,12 +1,21 @@
-vim.cmd [[packadd packer.nvim]]
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
 
 local packer = require('packer')
 
 packer.startup(function(use)
 	use 'wbthomason/packer.nvim'
-	use 'Shatur/neovim-ayu'
-	use { "catppuccin/nvim", as = "catppuccin" }
-	use 'shaunsingh/nord.nvim'
+	use 'folke/tokyonight.nvim'
 	use {
 		'nvim-tree/nvim-tree.lua',
 		requires = {
