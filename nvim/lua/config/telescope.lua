634f00e27f42fc09d31fcc4b9da7043b856dee19
phuwanat.r <ggamegg170@gmail.com>
2024-03-03 14:31:16 +0700

    Open from buffer

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -14,5 +14,5 @@ require('telescope').setup{
 vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<CR>')
 vim.keymap.set('n', '<leader>fg', '<cmd>Telescope git_files<CR>')
 vim.keymap.set('n', '<leader>fl', '<cmd>Telescope live_grep<CR>')
-vim.keymap.set('n', '<leader>fb', '<cmd>Telescope file_browser<CR>')
+vim.keymap.set('n', '<leader>fb', '<cmd>Telescope file_browser path=%:p:h select_buffer=true<CR>')
 vim.keymap.set('n', '<leader>fh', '<cmd>Telescope help_tags<CR>')
