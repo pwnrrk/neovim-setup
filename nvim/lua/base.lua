9f6e4c308d990c915a573f4dd06c955c9c95be7f
pttdigital\zphuwanat.r <zphuwanat.r@pttdigital.com>
2025-09-25 14:29:06 +0700

    Update for linux

--- nvim/lua/base.lua
+++ nvim/lua/base.lua
@@ -14,11 +14,15 @@ vim.opt.hlsearch = false
 vim.opt.smartindent = true
 vim.opt.foldmethod = 'indent'
 vim.opt.foldenable = false
-vim.opt.clipboard:prepend { 'unnamed', 'unnamedplus' }
+
+vim.opt.clipboard = "unnamedplus"
+
 if vim.fn.has('win32') then
 	vim.opt.shell = "pwsh"
 	vim.opt.shellcmdflag = '-nologo -noprofile -ExecutionPolicy RemoteSigned -command'
 	vim.opt.shellxquote = ''
 end
+
 vim.keymap.set('n', '<SPACE>', '<Nop>')
 vim.g.mapleader = ' '
+vim.cmd.colorscheme("github_dark_default")
