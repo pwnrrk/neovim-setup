c93efdabe886043a9a1ab0210b1a8ff19f93c0f3
R.Phuwanat <r.phuwanat.dev@gmail.com>
2026-07-05 22:39:45 +0700

    Fallback to github theme

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,3 +1,18 @@
+-- Github
+return {
+  'projekt0n/github-nvim-theme',
+  name = 'github-theme',
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
+  config = function()
+    require('github-theme').setup({
+      -- ...
+    })
+
+    vim.cmd('colorscheme github_dark_default')
+  end,
+}
+
 -- Onedark
 -- return {
 --   {
@@ -15,19 +30,19 @@
 -- }
 
 -- Catppuccin
-return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      require("catppuccin").setup({
-        flavour = "macchiato"
-      })
-      vim.cmd.colorscheme "catppuccin"
-    end
-  }
-}
+-- return {
+--   {
+--     "catppuccin/nvim",
+--     name = "catppuccin",
+--     priority = 1000,
+--     config = function()
+--       require("catppuccin").setup({
+--         flavour = "macchiato"
+--       })
+--       vim.cmd.colorscheme "catppuccin"
+--     end
+--   }
+-- }
 
 -- VS Code
 -- return {
