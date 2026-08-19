e1b33ce57ea24df4c2e781c92a297598f21b1b6d
R.Phuwanat <r.phuwanat.dev@gmail.com>
2026-08-19 20:45:44 +0700

    Add tokyo night theme

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,15 +1,26 @@
 -- Github
-return {
-  'projekt0n/github-nvim-theme',
-  name = 'github-theme',
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
-  config = function()
-    require('github-theme').setup({
-      -- ...
-    })
+-- return {
+--   'projekt0n/github-nvim-theme',
+--   name = 'github-theme',
+--   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+--   priority = 1000, -- make sure to load this before all the other start plugins
+--   config = function()
+--     require('github-theme').setup({
+--       -- ...
+--     })
+--
+--     vim.cmd('colorscheme github_dark_default')
+--   end,
+-- }
 
-    vim.cmd('colorscheme github_dark_default')
+-- Tokyo Night
+return {
+  'folke/tokyonight.nvim',
+  name = 'tokyo-night',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.cmd('colorscheme tokyonight')
   end,
 }
 
