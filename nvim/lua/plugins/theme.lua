5909e77ce4358b13a0f824559aefc34b9e8e8fd8
R.Phuwanat <r.phuwanat.dev@gmail.com>
2026-09-23 17:26:54 +0700

    Add theme switcher

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,105 +1,112 @@
--- Github
--- return {
---   'projekt0n/github-nvim-theme',
---   name = 'github-theme',
---   lazy = false, -- make sure we load this during startup if it is your main colorscheme
---   priority = 1000, -- make sure to load this before all the other start plugins
---   config = function()
---     require('github-theme').setup({
---       -- ...
---     })
---
---     vim.cmd('colorscheme github_dark_default')
---   end,
--- }
+local data_file = vim.fn.stdpath("state") .. "/theme.txt"
 
--- Tokyo Night
--- return {
---   'folke/tokyonight.nvim',
---   name = 'tokyo-night',
---   lazy = false,
---   priority = 1000,
---   config = function()
---     vim.cmd('colorscheme tokyonight')
---   end,
--- }
+local function read_theme()
+  local f = io.open(data_file, "r")
+  if not f then return "catppuccin" end
+  local name = f:read("*l")
+  f:close()
+  return (name and name ~= "") and name or "catppuccin"
+end
 
--- Onedark
--- return {
---   {
---     "navarasu/onedark.nvim",
---     priority = 1000, -- load before everything else
---     config = function()
---       require("onedark").setup({
---         style = "dark", -- dark | darker | cool | deep | warm | warmer
---         transparent = false, -- set true if you want Hyprland transparency
---         term_colors = true,
---       })
---       require("onedark").load()
---     end,
---   },
--- }
+local function write_theme(name)
+  local f = io.open(data_file, "w")
+  if f then
+    f:write(name)
+    f:close()
+  end
+end
 
--- Catppuccin
--- return {
---   {
---     "catppuccin/nvim",
---     name = "catppuccin",
---     priority = 1000,
---     config = function()
---       require("catppuccin").setup({
---         flavour = "macchiato"
---       })
---       vim.cmd.colorscheme "catppuccin"
---     end
---   }
--- }
-
--- VS Code
--- return {
---   {
---     "Mofiqul/vscode.nvim",
---     name = "vscode",
---     priority = 1000,
---     config = function ()
---       require("vscode").setup()
---       vim.cmd.colorscheme "vscode"
---     end
---   }
--- }
-
--- Ayu
--- return {
---   {
---     "Shatur/neovim-ayu",
---     name = "ayu",
---     priority = 1000,
---     config = function ()
---       require("ayu").setup({})
---       vim.cmd.colorscheme "ayu-dark"
---     end
---   }
--- }
-
--- Base 16
-return {
-  "tinted-theming/tinted-nvim",
-  priority = 1000,
-  lazy = false,
-  opts = {
-    default_scheme = "base16-tomorrow-night",
+local themes = {
+  catppuccin = {
+    plugin = "catppuccin",
+    spec = { "catppuccin/nvim", name = "catppuccin" },
+    apply = function()
+      require("catppuccin").setup({ flavour = "mocha" })
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
+  tokyonight = {
+    plugin = "tokyo-night",
+    spec = { "folke/tokyonight.nvim", name = "tokyo-night" },
+    apply = function()
+      vim.cmd.colorscheme("tokyonight")
+    end,
+  },
+  github_dark_default = {
+    plugin = "github-theme",
+    spec = { "projekt0n/github-nvim-theme", name = "github-theme" },
+    apply = function()
+      require("github-theme").setup({})
+      vim.cmd.colorscheme("github_dark_default")
+    end,
+  },
+  onedark = {
+    plugin = "onedark.nvim",
+    spec = { "navarasu/onedark.nvim" },
+    apply = function()
+      require("onedark").setup({ style = "dark", transparent = false, term_colors = true })
+      require("onedark").load()
+    end,
+  },
+  vscode = {
+    plugin = "vscode",
+    spec = { "Mofiqul/vscode.nvim", name = "vscode" },
+    apply = function()
+      require("vscode").setup()
+      vim.cmd.colorscheme("vscode")
+    end,
+  },
+  ayu = {
+    plugin = "neovim-ayu",
+    spec = { "Shatur/neovim-ayu" },
+    apply = function()
+      require("ayu").setup({})
+      vim.cmd.colorscheme("ayu-dark")
+    end,
+  },
+  nord = {
+    plugin = "nord.nvim",
+    spec = { "shaunsingh/nord.nvim", name = "nord" },
+    apply = function()
+      vim.cmd.colorscheme("nord")
+    end,
   },
 }
 
--- Nord
--- return {
---   {
---     'shaunsingh/nord.nvim',
---     name = "nord",
---     priority = 1000,
---     config = function ()
---       vim.cmd.colorscheme "nord"
---     end
---
---   }
--- }
+local current = read_theme()
+if not themes[current] then current = "catppuccin" end
+
+vim.api.nvim_create_user_command("Theme", function(opts)
+  local name = opts.args
+  if name == "" then
+    print("Current: " .. current .. " | Available: " .. table.concat(vim.tbl_keys(themes), ", "))
+    return
+  end
+  local t = themes[name]
+  if not t then
+    vim.notify("Unknown theme: " .. name, vim.log.levels.ERROR)
+    return
+  end
+  require("lazy").load({ plugins = { t.plugin } })
+  t.apply()
+  current = name
+  write_theme(name)
+end, {
+  nargs = "?",
+  complete = function() return vim.tbl_keys(themes) end,
+})
+
+local specs = {}
+for name, t in pairs(themes) do
+  local spec = vim.deepcopy(t.spec)
+  if name == current then
+    spec.lazy = false
+    spec.priority = 1000
+    spec.config = t.apply
+  else
+    spec.lazy = true
+  end
+  table.insert(specs, spec)
+end
+
+return specs
